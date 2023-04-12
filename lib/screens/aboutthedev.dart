aa7e533ef197b9cc1851628eea1411bbb23f3da9
Debjit Sarkar <sarkarbabudebjit@gmail.com>
2023-04-12 23:32:25 +0530

    ui

--- lib/screens/aboutthedev.dart
+++ lib/screens/aboutthedev.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:wallpaper_application/constants/projectColors.dart';
 
 class AboutTheDev extends StatefulWidget {
   const AboutTheDev({super.key});
@@ -12,6 +13,50 @@ class AboutTheDev extends StatefulWidget {
 class _AboutTheDevState extends State<AboutTheDev> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: bgcolor,
+      appBar: AppBar(
+        title: Text(
+          'Pixilate',
+          style: TextStyle(fontWeight: FontWeight.w800, fontSize: 22),
+        ),
+        //leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/appbarbg2.jpeg"),
+                  fit: BoxFit.cover)),
+        ),
+        elevation: 5,
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 30,
+          ),
+          //circular image
+          CircleAvatar(
+            backgroundImage: AssetImage("assets/images/demo.jpg"),
+            radius: 60,
+          ),
+
+          SizedBox(
+            height: 20,
+          ),
+          //Main Text
+          Container(
+            width: double.infinity,
+            alignment: Alignment.center,
+            child: Text(
+              "Debjit",
+              style: TextStyle(fontSize: 20, color: Colors.white),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
